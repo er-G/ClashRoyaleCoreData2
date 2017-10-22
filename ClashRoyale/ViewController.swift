4c48a9efaef24bc33ffdd88fabd084b12fd00b9d
Jorge MR <jorge_mr@icloud.com>
2017-10-22 14:24:55 -0500

    Initial Commit

---
+++ ClashRoyale/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  ClashRoyale
+//
+//  Created by Jorge MR on 22/10/17.
+//  Copyright © 2017 none. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        //button.titleLabel?.numberOfLines = 0
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func Agregar(_ sender: UIButton) {
+    }
+    
+    @IBAction func Buscar(_ sender: UIButton) {
+    }
+    
+    @IBAction func Coleccion(_ sender: UIButton) {
+    }
+    
+    @IBAction func Borrar(_ sender: UIButton) {
+    }
+    
+}
+
